eac369c0ab39a51e7300e6c8299b1cc0e695d862
Dogpa <dogpa0130@gmail.com>
2022-02-28 13:10:45 +0800

    Initial Commit

---
+++ Swift Practice 136 SwiftUI Picker Image/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 136 SwiftUI Picker Image
+//
+//  Created by Dogpa's MBAir M1 on 2022/2/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 136 SwiftUI Picker Image/Swift_Practice_136_SwiftUI_Picker_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_136_SwiftUI_Picker_ImageApp.swift
+//  Swift Practice 136 SwiftUI Picker Image
+//
+//  Created by Dogpa's MBAir M1 on 2022/2/28.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_136_SwiftUI_Picker_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
